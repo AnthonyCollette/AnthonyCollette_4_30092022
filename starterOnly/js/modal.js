e3ad6b40427a85f815315f4435d8c41c8b086ba3
Anthony Collette <100pour100inde@gmail.com>
2022-09-30 15:52:03 +0200

    close modal

--- starterOnly/js/modal.js
+++ starterOnly/js/modal.js
@@ -1,16 +1,9 @@
-// function editNav() {
-//   var x = document.getElementById("myTopnav");
-//   if (x.className === "topnav") {
-//     x.className += " responsive";
-//   } else {
-//     x.className = "topnav";
-//   }
-// }
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const burgerBtn = document.getElementById('burger-btn');
+const closeBtn = document.getElementById('close-btn');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -19,8 +12,6 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 function launchModal() {
   modalbg.style.display = "block";
 }
-
-let burgerBtn = document.getElementById('burger-btn')
 
 // Affichage et désaffichage du menu mobile au click
 burgerBtn.addEventListener('click', () => {
@@ -31,3 +22,6 @@ burgerBtn.addEventListener('click', () => {
     x.className = "topnav";
   }
 })
+
+// Fermeture du modal
+closeBtn.addEventListener('click', () => modalbg.style.display = "none")
